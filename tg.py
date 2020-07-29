90d1a04be33878f1c6068d857deb1cfaf0474cd5
bebebe-cmd <b@ysx.in>
2020-07-29 04:44:56 +0300

    adding telegram

--- tg.py
+++ tg.py
@@ -102,12 +102,14 @@ def checkuser(uid):
 
 
 def create(update, context):
-    #userid = update.message.chat.id
+    userid = update.message.chat.id
 
     print(update)
     print('\n\n')
-
     print(context)
+    print('\n\n')
+    print(userid)
+
     dtime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
 
     userdata = checkuser(userid)
