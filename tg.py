464bd62feb3a1589f2461d7c2a54d94ff87f29d6
bebebe-cmd <b@ysx.in>
2020-07-29 03:07:38 +0300

    making stuff usable

--- tg.py
+++ tg.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters, RegexHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from telegram import Bot as tgBot
+import shelve
 
 import datetime
 import os
@@ -21,6 +22,8 @@ elif loglevel == "INFO":
 else:
     logging.basicConfig(filename=logpath, level=logging.WARNING, format='%(asctime)s %(message)s')
 
+
+### Bot stuff
 
 def sendTele(recv, msg):
     bot = tgBot(teletoken)
@@ -55,12 +58,41 @@ def processtext(bot, update):
     print(dtime + " - " + str(userid) + ' sent text message:\n' + str(text))
 
 
+### VM control
+
+def create(bot, update):
+    userid = update.message.from_user.id
+    dtime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+
+
+def destroy(bot, update):
+    userid = update.message.from_user.id
+    dtime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+
+
+def restart(bot, update):
+    userid = update.message.from_user.id
+    dtime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+
+
+def listvms(bot, update):
+    userid = update.message.from_user.id
+    dtime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+
+
 def initbot():
-    usertext = MessageHandler(Filters.text, processtext)
+
+    usertext = MessageHandler(Filters.private, processtext)
     userphoto = MessageHandler(Filters.photo, processphoto)
 
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('myid', myid))
+
+    updater.dispatcher.add_handler(CommandHandler('create', create))
+    updater.dispatcher.add_handler(CommandHandler('destroy', destroy))
+    updater.dispatcher.add_handler(CommandHandler('restart', restart))
+    updater.dispatcher.add_handler(CommandHandler('list', listvms))
+
 
     dispatcher.add_handler(usertext)
     dispatcher.add_handler(userphoto)
