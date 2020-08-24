da42e92e8876fffa2fd730cd639e7d4c0feb3453
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2020-08-24 22:05:30 +0300

    Update cbsd.py

--- cbsd.py
+++ cbsd.py
@@ -1,11 +1,11 @@
 from helpers import scp, hostcmd
 
 def bcreate(jconf):
-    scp(jconf,'/tmp/vm.jconf')
-    hostcmd('sudo -u root cbsd bcreate jconf=/tmp/vm.jconf')
+	scp(jconf,'/tmp/vm.jconf')
+	hostcmd('sudo -u root cbsd bcreate jconf=/tmp/vm.jconf')
 def bstart(vm_name):
 	hostcmd('sudo -u root cbsd bstart ' + vm_name)
 def bremove(vm_name):
-    hostcmd('sudo -u root cbsd bremove ' + vm_name)
+	hostcmd('sudo -u root cbsd bremove ' + vm_name)
 def brestart(vm_name):
-    hostcmd('sudo -u root cbsd brestart ' + vm_name)
+	hostcmd('sudo -u root cbsd brestart ' + vm_name)
