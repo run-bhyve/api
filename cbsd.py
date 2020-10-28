cfcb5c5e558cabcda2ad9a6f7d01a8118e54a6e9
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2020-10-28 09:22:34 +0300

    Update cbsd.py

--- cbsd.py
+++ cbsd.py
@@ -1,18 +1,27 @@
 from helpers import hostcp, hostcmd
+import sqlite3
 
 # Create virtual machine with config `jconfig`
 def bcreate(jconf):
 	hostcp(jconf,'/tmp/vm.jconf')
-	hostcmd('sudo -u root cbsd bcreate jconf=/tmp/vm.jconf')
+	hostcmd('sudo cbsd bcreate jconf=/tmp/vm.jconf')
 
 # Start virtual machine
 def bstart(vm_name):
-	hostcmd('sudo -u root cbsd bstart ' + vm_name)
+	hostcmd('sudo cbsd bstart ' + vm_name)
 
 # Destroy virtual machine
 def bremove(vm_name):
-	hostcmd('sudo -u root cbsd bremove ' + vm_name)
+	hostcmd('sudo cbsd bremove ' + vm_name)
 
 # Restart virtual machine
 def brestart(vm_name):
-	hostcmd('sudo -u root cbsd brestart ' + vm_name)
+	hostcmd('sudo cbsd brestart ' + vm_name)
+
+# Get list of nodes
+# https://docs.python.org/2/library/sqlite3.html
+def get_nodes():
+	conn = sqlite3.connect('/db/nodes.sqlite')
+	c = conn.cursor()
+	c.execute("SELECT * FROM nodelist")
+	return c
