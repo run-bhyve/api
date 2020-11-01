83dc2003b19dbe3d888c57c0c140147209daea82
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2020-11-01 17:09:20 +0300

    Update cbsd.py

--- cbsd.py
+++ cbsd.py
@@ -11,31 +11,23 @@ visual_params={"header":0}
 
 # Run any command on cluster
 def cbsd_exec(subcmd, params={}):
-	if params.jname:
-		# Operate only with VMs with prefix 'api_'
-		params.jname = 'api_' + params.jname
-	# Apply output format
-	params.update(visual_params)
 	cmd = {"Command": subcmd, "CommandArgs": params}
 	cmd = json.dumps(cmd)
-	return client.put(cmd)
-
-# Get operation`s result
-def get_result(id):
-	True
+	return cmd
+	#return client.put(cmd)
 
 # Create virtual machine
 def bcreate(jconf):
-	cbsd_exec('bcreate', jconf)
+	return cbsd_exec('bcreate', jconf)
 
 # Start virtual machine
 def bstart(jname):
-	cbsd_exec('bstart', {"jname"=jname})
+	return cbsd_exec('bstart', {"jname": jname})
 
 # Destroy virtual machine
 def bremove(jname):
-	cbsd_exec('bstart', {"jname"=jname})
+	return cbsd_exec('bstart', {"jname": jname})
 
 # Restart virtual machine
 def brestart(jname):
-	cbsd_exec('brestart', {"jname"=jname})
+	cbsd_exec('brestart', {"jname": jname})
