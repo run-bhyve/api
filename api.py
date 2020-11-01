0ba59ef2ffad18c2af32115cb288187d127fb49c
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2020-11-01 17:09:23 +0300

    Update api.py

--- api.py
+++ api.py
@@ -7,49 +7,22 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class VPS(BaseModel):
-    name: str
-    price: float
-    cpu: int
-    ram: int
-    drive: int
+    id: int
+    jname: str
+    vm_cpus: int
+    vm_ram: int
+    imgsize: int
     profile: str
+    vm_os_profile: str
 
 @app.post('/vps/')
 def create_vps(vps: VPS):
-    if vps.profile in ['debian', 'centos']:
-        vm_ip4addr = hostreadcmd('sudo cbsd dhcpd')
-
-        jconf_template = 'vm_linux.jconf'
-        jconf_tmp = '/tmp/vm.jconf'
-
-        if image == 'debian':
-            vm_profile = 'cloud-Debian-x86-10'
-        elif image == 'centos':
-            vm_profile = 'cloud-CentOS-8.2-x86_64'
-
-        conf = {
-            'vm_name': vm_name,
-            'ip': vm_ip4addr,
-            'vm_user_name': 'linux',
-            'vm_user_pwd': randstr(),
-            'vm_root_pwd': randstr(),
-            'vm_profile': vm_profile,
-        }
-        with open(jconf_tmp, "w") as f:
-            f.write(render_template(jconf_template,**conf))
-        # Create from *local* configuration file
-        cbsd.bcreate(jconf_tmp)
-        cbsd.bstart(vm_name)
-        return conf
-    else:
-        return {'error': 'no such image'}
+    return cbsd.bcreate(vps)
 
 @app.delete('/vps/<vm_name>')
 def destroy_vps(vm_name):
-    cbsd.bremove(vm_name)
-    return {"status": "ok"}
+    return cbsd.bremove(vm_name)
 
 @app.patch('/vps/<vm_name>/restart')
 def restart_vps(vm_name):
-    cbsd.brestart(vm_name)
-    return {"status": "ok"}
+    return cbsd.brestart(vm_name)
